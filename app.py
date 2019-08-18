a8f85528a113bfe38f56ed9c7dd35d4e93e22bda
daQuincy <yuquan95@gmail.com>
2019-08-18 17:35:13 +0800

    updated with cleaner code

---
+++ app.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 16:23:49 2019
+
+@author: YQ
+"""
+
+from utils import predict
+from tensorflow.keras.models import load_model
+from datetime import datetime
+from flask import Flask, render_template, request
+from tensorflow.python.keras.backend import set_session
+
+import json
+import tweepy
+import pickle
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+
+app = Flask(__name__)
+keys = json.load(open("keys.json"))
+tokenizer = pickle.load(open("tokenizer.p", "rb"))
+
+global graph  # https://towardsdatascience.com/deploying-keras-deep-learning-models-with-flask-5da4181436a2
+graph = tf.get_default_graph()
+sess = tf.Session()
+set_session(sess)  # https://github.com/tensorflow/tensorflow/issues/28287
+model = load_model("twitter.h5")
+
+
+def get_user_timeline(username):
+    """
+    parameters:
+    username: string
+    returns:
+    api.user_timeline(username): list
+    """
+    
+    auth = tweepy.OAuthHandler(keys["consumer_key"], keys["consumer_secret"])
+    auth.set_access_token(keys["access_key"], keys["access_secret"])
+    api = tweepy.API(auth)
+    
+    return api.user_timeline(username)
+
+def make_prediction(timeline, start, end):
+    # process start and end date
+    start = list(map(int, start.split('-')))
+    start = datetime(start[0], start[1], start[2], 0, 0, 0)
+    end = list(map(int, end.split('-')))
+    end = datetime(end[0], end[1], end[2], 23, 59, 59)
+
+    analyzed_tweets = pd.DataFrame(columns=["Datetime", "Tweet", "Sentiment", "Score"])
+    date = []
+    tweets = []
+    for tweet in timeline:
+        if tweet.created_at >= start and tweet.created_at <= end:
+            date.append(tweet.created_at)
+            tweets.append(tweet.text)
+        elif tweet.created_at < start:
+            break
+
+    with graph.as_default():
+        set_session(sess)
+        predictions = predict(model, tokenizer, tweets)
+
+    analyzed_tweets["Score"] = np.squeeze(predictions["score"], -1)
+    analyzed_tweets["Sentiment"] = predictions["label"]
+    analyzed_tweets["Datetime"] = date
+    analyzed_tweets["Tweet"] = tweets
+
+    return analyzed_tweets
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        start = request.form['from']
+        end = request.form['to']
+
+        timeline = get_user_timeline(username)
+        table = make_prediction(timeline, start, end)
+
+        return render_template('index.html', tables=[table.to_html()], titles=table.columns.values)
+    else:
+        return render_template('index.html')
+    
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
---
+++ utils.py
@@ -0,0 +1,61 @@
+import time
+import re
+import numpy as np
+from nltk.stem import SnowballStemmer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+stemmer = SnowballStemmer("english")
+TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
+
+# SENTIMENT
+POSITIVE = "POSITIVE"
+NEGATIVE = "NEGATIVE"
+NEUTRAL = "NEUTRAL"
+SENTIMENT_THRESHOLDS = (0.4, 0.7)
+
+def preprocess(text, stem=False):
+    # Remove link,user and special characters
+    text = re.sub(TEXT_CLEANING_RE, ' ', str(text).lower()).strip()
+    tokens = []
+    for token in text.split():
+        if stem:
+            tokens.append(stemmer.stem(token))
+        else:
+            tokens.append(token)
+    return " ".join(tokens)
+    
+    
+def decode_sentiment(score, include_neutral=True):
+    if include_neutral:        
+        label = NEUTRAL
+        if score <= SENTIMENT_THRESHOLDS[0]:
+            label = NEGATIVE
+        elif score >= SENTIMENT_THRESHOLDS[1]:
+            label = POSITIVE
+
+        return label
+    else:
+        return NEGATIVE if score < 0.5 else POSITIVE    
+    
+    
+def predict(model, tokenizer, text, include_neutral=True, sequence_length=300):
+    """
+    model: Keras model
+    tokenizer: Tokenizer object
+    text: List of strings (tweets)
+    """
+    
+    start_at = time.time()
+    # Tokenize text
+    text = [preprocess(t) for t in text]
+    X = pad_sequences(tokenizer.texts_to_sequences(text), maxlen=sequence_length)
+    # Predict
+    score = model.predict(X)
+    # Decode sentiment
+    labels = []
+    for s in score:
+        label = decode_sentiment(s, include_neutral=include_neutral)
+        labels.append(label)
+
+    return {"label": labels, "score": score,
+       "elapsed_time": time.time()-start_at}      
